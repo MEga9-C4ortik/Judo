66c8c4697a17d721fbd551a169b89d0152c1abd0
Mega <megaovezov9@gmail.com>
2026-05-02 14:48:41 +0800

    fix

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,16 +2,16 @@ import styles from "./Card.module.css";
 
 function Card({nameEN,nameJP,category,description,youtubeID}) {
     return (
-        <div className={"styles.card"}>
-            <img className={"styles.img"} src={`https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`} />
-            <div className={"styles.body"}>
-                <span className={"styles.name"}>
+        <div className={styles.card}>
+            <img className={styles.img} src={`https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`} />
+            <div className={styles.body}>
+                <span className={styles.name}>
                     <p> {nameEN} </p>
                     <p> {nameJP} </p>
                 </span>
-                <p className={"styles.category"}> {category}</p>
-                <p className={"styles.description"}> {description}</p>
-                <a className={"styles.link"} href={"https://www.youtube.com/watch?v="}> {youtubeID} </a>
+                <p className={styles.category}> {category}</p>
+                <p className={styles.description}> {description}</p>
+                <a className={styles.link} href={"https://www.youtube.com/watch?v="}> {youtubeID} </a>
             </div>
         </div>
     )
