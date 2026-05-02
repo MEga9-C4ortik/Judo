72da62a949b0ef21e7e6f289b1d522667e375bc9
Mega <megaovezov9@gmail.com>
2026-05-02 14:41:35 +0800

    created Card css file, fix Card classnames

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,17 @@
+import styles from "./Card.module.css";
+
 function Card({nameEN,nameJP,category,description,youtubeID}) {
     return (
-        <div className="card">
-            <img src={`https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`} />
-            <div className="body">
-                <span className="name">
+        <div className={"styles.card"}>
+            <img className={"styles.img"} src={`https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`} />
+            <div className={"styles.body"}>
+                <span className={"styles.name"}>
                     <p> {nameEN} </p>
                     <p> {nameJP} </p>
                 </span>
-                <p className="category"> {category}</p>
-                <p className="description"> {description}</p>
-                <a className="link"> {youtubeID} </a>
+                <p className={"styles.category"}> {category}</p>
+                <p className={"styles.description"}> {description}</p>
+                <a className={"styles.link"} href={"https://www.youtube.com/watch?v="}> {youtubeID} </a>
             </div>
         </div>
     )
