75850041afb75d14e336d80ea80df3484afeec2d
Mega <megaovezov9@gmail.com>
2026-05-02 16:14:47 +0800

    trying to fix card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,12 +3,12 @@ import styles from "./Card.module.css";
 function Card({nameEN,nameJP,category,description,youtubeID}) {
     return (
         <div className={styles.card}>
-            <img className={styles.img} src={`https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`} />
+            <img className={styles.img} src={`https://img.youtube.com/vi/${youtubeID}/hqdefault.jpg`} />
             <div className={styles.body}>
-                <span className={styles.name}>
+                <div className={styles.name}>
                     <p> {nameEN} </p>
                     <p> {nameJP} </p>
-                </span>
+                </div>
                 <p className={styles.category}> {category}</p>
                 <p className={styles.description}> {description}</p>
                 <a className={styles.link} href={`https://www.youtube.com/watch?v=${youtubeID}`} target="_blank" rel="noopener noreferrer">
