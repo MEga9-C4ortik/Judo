baf818128d14e7b5cd20f20e977d51c2561cdc27
Mega <megaovezov9@gmail.com>
2026-05-10 11:11:10 +0800

    fix library DOM

--- src/Pages/Library.jsx
+++ src/Pages/Library.jsx
@@ -11,32 +11,35 @@ function Library() {
         .filter(t => !selectedCategory || t.category === selectedCategory);
 
     return (
-        <div className={styles.Library}>
-            <div className={styles.filters}>
-                <button className={selectedCategory ? styles.active : styles.btn}
-                        onClick={() => setCategory("")}>
-                    All
-                </button>
-
-                {categories.map(t =>
-                    <button className={selectedCategory === t ? styles.active : styles.btn}
-                            key={t}
-                            onClick={() => setCategory(t)}>
-                        {t}
+        <>
+            <div className={styles.controls}>
+                <div className={styles.filters}>
+                    <button className={selectedCategory ? styles.active : styles.btn}
+                            onClick={() => setCategory("")}>
+                        All
                     </button>
-                )}
-            </div>
 
-            {filtered.map(technique =>
-                <Card
-                    key={technique.id}
-                    nameEN={technique.nameEN}
-                    nameJP={technique.nameJP}
-                    category={technique.category}
-                    description={technique.description}
-                    youtubeID={technique.youtubeID}
-                />)}
-        </div>
+                    {categories.map(t =>
+                        <button className={selectedCategory === t ? styles.active : styles.btn}
+                                key={t}
+                                onClick={() => setCategory(t)}>
+                            {t}
+                        </button>
+                    )}
+                </div>
+            </div>
+            <div className={styles.Library}>
+                {filtered.map(technique =>
+                    <Card
+                        key={technique.id}
+                        nameEN={technique.nameEN}
+                        nameJP={technique.nameJP}
+                        category={technique.category}
+                        description={technique.description}
+                        youtubeID={technique.youtubeID}
+                    />)}
+            </div>
+        </>
     );
 }
 
